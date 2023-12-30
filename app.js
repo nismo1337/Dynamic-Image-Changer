9c23d0f12c90c6a2da59b2cda418115860bfd2ca
nismo <61671297+nismo1337@users.noreply.github.com>
2023-12-30 05:18:48 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 const app = express();
 
-const imageDir = path.join(__dirname, "images"); // Absolute path to the image directory
+const imageDir = path.join(__dirname, "images"); 
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Random Image Generator!");
@@ -27,11 +27,11 @@ app.get("/meow.png", (req, res) => {
     const randomIndex = Math.floor(Math.random() * imageFiles.length);
     const randomImage = path.join(imageDir, imageFiles[randomIndex]);
 
-    res.sendFile(randomImage); // Remove the { root: "/" } option
+    res.sendFile(randomImage); 
   });
 });
 
-const port = process.env.PORT || 3000; // Use the dynamic port from Heroku or 3000 locally
+const port = process.env.PORT || 3000; 
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
